561967868e61973db4779cfd8a04637b44de710b
Wenfeng CAI <wenfeng.cai@demc.com.cn>
2017-05-03 16:38:46 +0800

    Change settings for opencv package
    The new package name `libopencv-dev` is compatible to Debian/Ubuntu
    system. The `requires` option could not be properly provided. So make
    sure to set this field in the following dialog.

--- opencv.sh
+++ opencv.sh
@@ -2,9 +2,12 @@
 
 # Maintainer: [ http://opencv.org/ ]
 # Summary: [ Open Source Computer Vision Library ]
-# Name:    [ opencv ]
+# Name:    [ libopencv-dev ]
 # Version: [ 3.2.0 ]
 # License: [ BSD ]
 # Group:   [ libdevel ]
-# Provides: [ opencv ]
-checkinstall --nodoc --install=no --fstrans=yes --pkgname=opencv --pkgversion=3.2.0 --pkglicense=BSD --pkggroup=libdevel --maintainer=http://opencv.org/ --provides=opencv
+# Provides: [  ]
+checkinstall --nodoc --install=no --fstrans=yes \
+--pkgname=libopencv-dev --pkgversion=3.2.0 --pkglicense=BSD \
+--pkggroup=libdevel --maintainer=http://opencv.org/ --backup=no
+# --requires='libavcodec53 (>= 4:0.8-1~), libavformat53 (>= 4:0.8-1~), libavutil51 (>= 4:0.8-1~), libc6 (>= 2.15), libdc1394-22, libgcc1 (>= 1:4.1.1), libglib2.0-0 (>= 2.31.2), libgtk2.0-0 (>= 2.8.0), libilmbase6 (>= 1.0.1), libjasper1, libjpeg8 (>= 8c), libopenexr6 (>= 1.6.1), libpng12-0 (>= 1.2.13-4), libstdc++6 (>= 4.4.0), libswscale2 (>= 4:0.8-1~), libtiff4, libv4l-0 (>= 0.5.0), zlib1g (>= 1:1.1.4)'
